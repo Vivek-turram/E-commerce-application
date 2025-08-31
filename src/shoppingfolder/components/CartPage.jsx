f810e233e4f9de3427f1151a370ebadf7917b1cb
Vivek-turram <vivekturram719@gmail.com>
2025-09-01 01:01:33 +0530

    Update CartPage.jsx

--- src/shoppingfolder/components/CartPage.jsx
+++ src/shoppingfolder/components/CartPage.jsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React from "react";
 
-const CartPage = ({ cartItems, removeFromCart, totalPrice }) => {
+const CartPage = ({ cartItems, removeFromCart, totalPrice, addToCart }) => {
   return (
-    <div className="cart-container">
+    <div className="cart-container" style={{ padding: "20px" }}>
       <h2>CART</h2>
 
       {cartItems.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
         <div>
-      
           {cartItems.map((item) => (
-            <div key={item.id} style={{ marginBottom: '20px' }}>
+            <div
+              key={item.id} 
+            >
               <h3>{item.name}</h3>
-              <p>{item.price}</p>
-              <button onClick={() => removeFromCart(item.id)}>Remove from Cart</button>
+              <p>Price: {item.price}</p>
+
+              <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+                <button onClick={() => removeFromCart(item.id)}>Remove from cart</button>
+                <span>Quantity: {item.quantity}</span>
+                <button onClick={() => addToCart(item)}>+</button>
+              </div>
             </div>
           ))}
 
-          <div >
+          <div style={{ marginTop: "20px" }}>
             <h3>Total Price: {totalPrice} INR</h3>
           </div>
         </div>
