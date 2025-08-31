c2d67ae697003c6b71c4a81743f181fecb137046
Vivek-turram <vivekturram719@gmail.com>
2025-09-01 01:44:22 +0530

    Update Header.jsx

--- src/shoppingfolder/components/Header.jsx
+++ src/shoppingfolder/components/Header.jsx
@@ -1,27 +1,24 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaBars, FaTimes } from 'react-icons/fa'; // icons for hamburger & close
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Header = ({ cartItems }) => {
   const [menuOpen, setMenuOpen] = useState(false);
 
-  // calculate total quantity
+
   const totalItems = cartItems.reduce((sum, item) => sum + item.quantity, 0);
 
   return (
     <div className="headerSection">
 
-      {/* Left Side - Logo */}
+
       <div className="left">
         <h2>SHOPPING MALL</h2>
       </div>
 
-      {/* Hamburger Icon (visible only on mobile) */}
       <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
         {menuOpen ? <FaTimes /> : <FaBars />}
       </div>
-
-      {/* Center - Menu Bar */}
       <div className={`menuBar ${menuOpen ? "active" : ""}`}>
         <ul>
           <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
@@ -31,8 +28,6 @@ const Header = ({ cartItems }) => {
           <li><Link to="/signup" onClick={() => setMenuOpen(false)}>Signup</Link></li>
         </ul>
       </div>
-
-      {/* Right Side - Cart */}
       <div className="right">
         <div className="cart">
           <Link to="/cart">Cart ({totalItems})</Link>
