5e3f7f12da1435315408c869989b02f4ebbc6d50
Vivek-turram <vivekturram719@gmail.com>
2025-09-01 01:48:10 +0530

    Update Header.jsx

--- src/shoppingfolder/components/Header.jsx
+++ src/shoppingfolder/components/Header.jsx
@@ -5,13 +5,10 @@ import { FaBars, FaTimes } from 'react-icons/fa';
 const Header = ({ cartItems }) => {
   const [menuOpen, setMenuOpen] = useState(false);
 
-
   const totalItems = cartItems.reduce((sum, item) => sum + item.quantity, 0);
 
   return (
     <div className="headerSection">
-
-
       <div className="left">
         <h2>SHOPPING MALL</h2>
       </div>
@@ -19,15 +16,16 @@ const Header = ({ cartItems }) => {
       <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
         {menuOpen ? <FaTimes /> : <FaBars />}
       </div>
+
       <div className={`menuBar ${menuOpen ? "active" : ""}`}>
         <ul>
           <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
-          <li><Link to="/men" onClick={() => setMenuOpen(false)}>Men</Link></li>
           <li><Link to="/women" onClick={() => setMenuOpen(false)}>Women</Link></li>
-          <li><Link to="/signin" onClick={() => setMenuOpen(false)}>Signin</Link></li>
+          <li><Link to="/men" onClick={() => setMenuOpen(false)}>Men</Link></li>
           <li><Link to="/signup" onClick={() => setMenuOpen(false)}>Signup</Link></li>
         </ul>
       </div>
+
       <div className="right">
         <div className="cart">
           <Link to="/cart">Cart ({totalItems})</Link>
