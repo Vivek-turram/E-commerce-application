b9a97df41d0afadfc9e1b466495368ea8d5927dc
Vivek-turram <vivekturram719@gmail.com>
2025-08-31 23:25:00 +0530

    Update Header.jsx

--- src/shoppingfolder/components/Header.jsx
+++ src/shoppingfolder/components/Header.jsx
@@ -4,26 +4,29 @@ import { Link } from 'react-router-dom';
 const Header = ({ cartItems }) => {
   return (
     <div className='headerSection'>
+
       <div className="left">
         <div className="title">
           <h2>Shopping Mall</h2>
         </div>
       </div>
+
       <div className="center">
         <ul>
-          <li>Woman</li>
-          <li>Men</li>
-          <li>Children</li>
-          <li>Beauty</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/women">Women</Link></li>
+          <li><Link to="/men">Men</Link></li>
         </ul>
       </div>
+
       <div className="search">
         <input type="text" placeholder="Search..." />
       </div>
       <div className="right">
-        <div className="signin">Signin / Signup</div>
+        <div className="signin">
+          <Link to="/signin">Signin</Link> / <Link to="/signup">Signup</Link>
+        </div>
         <div className="cart">
-          {/* Add Link to Cart Page */}
           <Link to="/cart">Cart ({cartItems.length})</Link>
         </div>
       </div>
