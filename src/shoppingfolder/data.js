51f6928c0bcf361173d78c96aee5e3f73da10f21
Vivek-turram <vivekturram719@gmail.com>
2025-08-31 20:19:39 +0530

    Add files via upload

---
+++ src/shoppingfolder/data.js
@@ -0,0 +1,23 @@
+export const Gents = {
+  title: "Gents Fashion",
+  products: [
+    { id: 1, name: "White T-shirt", price: "559 INR", image: "assets/Men/1.jpg" },
+    { id: 2, name: "Black Shirt", price: "459 INR", image: "assets/Men/2.jpg" },
+    { id: 3, name: "Combo T-shirts", price: "999 INR", image: "assets/Men/3.jpg" },
+    { id: 4, name: "Denim Shirt", price: "759 INR", image: "assets/Men/4.jpg" },
+    { id: 5, name: "T-shirt", price: "859 INR", image: "assets/Men/5.jpg" },
+    { id: 6, name: "Casual Black shirt", price: "959 INR", image: "assets/Men/6.jpg" },
+  ],
+};
+
+export const Ladies = {
+  title: "Ladies Fashion",
+  products: [
+    { id: 1, name: "Orange top", price: "1559 INR", image: "assets/Woman/1.jpg" },
+    { id: 2, name: "Brown T-shirt", price: "1459 INR", image: "assets/Woman/2.jpg" },
+    { id: 3, name: "Orange-Tshirt", price: "1659 INR", image: "assets/Woman/3.jpg" },
+    { id: 4, name: "Black T-shirt", price: "1759 INR", image: "assets/Woman/4.jpg" },
+    { id: 5, name: "White T-shirt", price: "1859 INR", image: "assets/Woman/5.jpg" },
+    { id: 6, name: "Blue Shirt", price: "1959 INR", image: "assets/Woman/6.jpg" },
+  ],
+};
