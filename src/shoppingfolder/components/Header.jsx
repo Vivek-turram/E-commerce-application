26604865ab817c85cb03930e182fcaac56b264c5
Vivek-turram <vivekturram719@gmail.com>
2025-09-01 02:34:07 +0530

    Update Header.jsx

--- src/shoppingfolder/components/Header.jsx
+++ src/shoppingfolder/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaBars, FaTimes } from 'react-icons/fa';
-
+ 
   const Header = ({ cartItems }) => {
   const [menuOpen, setMenuOpen] = useState(false);
 
