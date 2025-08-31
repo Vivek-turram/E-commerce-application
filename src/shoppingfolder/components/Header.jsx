6989918a8cbdd31ec448c58cac17f6842bd89bbd
Vivek-turram <vivekturram719@gmail.com>
2025-09-01 01:50:38 +0530

    Update Header.jsx

--- src/shoppingfolder/components/Header.jsx
+++ src/shoppingfolder/components/Header.jsx
@@ -9,23 +9,28 @@ const Header = ({ cartItems }) => {
 
   return (
     <div className="headerSection">
+      {/* Left - Logo */}
       <div className="left">
         <h2>SHOPPING MALL</h2>
       </div>
 
+      {/* Hamburger (mobile only) */}
       <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
         {menuOpen ? <FaTimes /> : <FaBars />}
       </div>
 
+      {/* Menu Links */}
       <div className={`menuBar ${menuOpen ? "active" : ""}`}>
         <ul>
           <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
-          <li><Link to="/women" onClick={() => setMenuOpen(false)}>Women</Link></li>
           <li><Link to="/men" onClick={() => setMenuOpen(false)}>Men</Link></li>
+          <li><Link to="/women" onClick={() => setMenuOpen(false)}>Women</Link></li>
+          <li><Link to="/signin" onClick={() => setMenuOpen(false)}>Signin</Link></li>
           <li><Link to="/signup" onClick={() => setMenuOpen(false)}>Signup</Link></li>
         </ul>
       </div>
 
+      {/* Right - Cart */}
       <div className="right">
         <div className="cart">
           <Link to="/cart">Cart ({totalItems})</Link>
